2ae7a79aa4f91d88723fc5fab595d00787899613
Puffin & Turtle Software <puffinandturtle@gmail.com>
2016-08-02 11:08:45 +0100

    Updated for beta4 of iOS 10

--- Hello SceneKit .playground/Contents.swift
+++ Hello SceneKit .playground/Contents.swift
@@ -1,46 +1,51 @@
 
 import SceneKit
+import SpriteKit
 import PlaygroundSupport
 let pi2 = Float(M_PI_2)
 
-let view = SCNView(frame: CGRect(x: 0, y: 0, width: 400, height: 400))
-view.allowsCameraControl = true
-view.autoenablesDefaultLighting = true
 let scene = SCNScene()
-view.scene = scene
-PlaygroundPage.current.liveView = view
-
-let hello = SCNText(string: "Hello\nworld!", extrusionDepth: 3)
-hello.chamferRadius = 0.2
-//hello.flatness = 0.4
-//hello.font = UIFont(name: "Damascus Bold", size: 8)
+let hello = SCNText(string: "Hello\nworld!", extrusionDepth: 9)
+hello.chamferRadius = 0.5
+hello.flatness = 0.05
+hello.font = UIFont(name: "Superclarendon-Black", size: 12)
 let helloNode = SCNNode(geometry: hello)
 var min = SCNVector3Zero
 var max = SCNVector3Zero
-helloNode.getBoundingBoxMin(&min, max: &max)
+helloNode.__getBoundingBoxMin(&min, max: &max)
 print(min,max)
-// helloNode.position = SCNVector3Zero //(0, 0, 0) //SCNVector3(max.x * -0.5, max.y * -0.5, 0)
+ helloNode.position = SCNVector3((max.x - min.x) * -0.5, (max.y - min.y) * -0.5, 0) 
 //helloNode.scale = SCNVector3(0.2, 0.2, 0.2)
 scene.rootNode.addChildNode(helloNode)
 
-let camera = SCNNode()
-camera.camera = SCNCamera()
-camera.position = SCNVector3(16,12,40)
 
-scene.rootNode.addChildNode(camera)
+let cube = SCNNode(geometry: SCNBox(width: 30, height: 2, length: 8, chamferRadius: 0.5))
+cube.position = SCNVector3(-4, 3, 0)
+scene.rootNode.addChildNode(cube)
+let mat = SCNMaterial()
 
-/*
- let torus = SCNNode(geometry: SCNTorus(ringRadius: 5, pipeRadius: 2))
- torus.eulerAngles = SCNVector3(pi2,0,0)
- scene.rootNode.addChildNode(torus)
- */
-/*let light = SCNLight()
- light.type = SCNLightTypeOmni
+ mat.lightingModelName = SCNLightingModelPhysicallyBased
+mat.roughness.contents = UIColor.lightGray()
+mat.metalness.contents = SKTexture(noiseWithSmoothness: 0.8, size: CGSize(width: 500, height: 500), grayscale: true).cgImage()  //UIColor.darkGray()
+    
+ mat.diffuse.contents = #colorLiteral(red: 1.0, green: 0.498039215803146, blue: 0.756862759590149, alpha: 1.0)  
+cube.geometry?.materials = [mat] 
+hello.materials = [mat]
+
+ let light = SCNLight()
+light.type = SCNLightTypeOmni
  let lightNode = SCNNode()
  lightNode.light = light
- lightNode.position = SCNVector3(2,1,1)
+ lightNode.position = SCNVector3(8,12,15)
  scene.rootNode.addChildNode(lightNode)
- */
+ 
+let view = SCNView() 
+view.allowsCameraControl = true
+view.autoenablesDefaultLighting = true
+view.showsStatistics = true
+view.scene = scene
+view.backgroundColor = #colorLiteral(red: 0.0588235296308994, green: 0.180392161011696, blue: 0.24705882370472, alpha: 1.0)
+PlaygroundPage.current.liveView = view
 
 
 
